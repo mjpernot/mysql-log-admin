852363158c2a3ebd50e0bc032702d40d3587c1fd
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-12-17 14:57:47 +0000

    Update test/unit/mysql_log_admin/fetch_binlog.py

--- test/unit/mysql_log_admin/fetch_binlog.py
+++ test/unit/mysql_log_admin/fetch_binlog.py
@@ -35,6 +35,30 @@ import version
 __version__ = version.__version__
 
 
+class Popen(object):
+
+    """Class:  Popen
+
+    Description:  Class stub holder for subprocess.Popen class.
+
+    Methods:
+        __init__ -> Class initialization.
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+
+        """
+
+        self.stdout = ["binlog1"]
+
+
 class Server(object):
 
     """Class:  Server
@@ -71,7 +95,12 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
-        test_fetch_binlog -> Test with only default argument passed.
+        test_end_dt -> Test with end_dt argument passed.
+        test_start_dt -> Test with start_dt argument passed.
+        test_opt_arg_list -> Test with opt_arg_list argument passed.
+        test_binlog_files -> Test with binlog_files argument passed.
+        test_binpath -> Test with bin_path argument passed.
+        test_fetch_binlog -> Test with only default arguments passed.
 
     """
 
@@ -86,27 +115,128 @@ class UnitTest(unittest.TestCase):
         """
 
         self.server = Server()
-        self.filehandler = "FileHandler"
+        self.filehandler = ["binlog1"]
         self.binlog_files = [{"Log_name": "binlog1"}, {"Log_name": "binlog2"}]
+        self.opt_arg_list = ["--force-read", "--read-from-remote-server"]
+        self.start_dt = "start_datetime_format"
+        self.stop_dt = "end_datetime_format"
 
+    @mock.patch("mysql_log_admin.mysql_libs.fetch_logs")
+    @mock.patch("mysql_log_admin.subprocess.Popen")
+    def test_end_dt(self, mock_sub, mock_fetch):
+
+        """Function:  test_end_dt
+
+        Description:  Test with end_dt argument passed.
+
+        Arguments:
+
+        """
+
+        mock_sub.return_value = Popen()
+        mock_fetch.return_value = self.binlog_files
+
+        dataout = mysql_log_admin.fetch_binlog(
+            self.server, bin_path="./", start_dt=self.start_dt)
+
+        self.assertEqual([x for x in dataout], self.filehandler)
+
+    @mock.patch("mysql_log_admin.mysql_libs.fetch_logs")
+    @mock.patch("mysql_log_admin.subprocess.Popen")
+    def test_start_dt(self, mock_sub, mock_fetch):
+
+        """Function:  test_start_dt
+
+        Description:  Test with start_dt argument passed.
+
+        Arguments:
+
+        """
+
+        mock_sub.return_value = Popen()
+        mock_fetch.return_value = self.binlog_files
+
+        dataout = mysql_log_admin.fetch_binlog(
+            self.server, bin_path="./", start_dt=self.start_dt)
+
+        self.assertEqual([x for x in dataout], self.filehandler)
+
+    @mock.patch("mysql_log_admin.mysql_libs.fetch_logs")
+    @mock.patch("mysql_log_admin.subprocess.Popen")
+    def test_opt_arg_list(self, mock_sub, mock_fetch):
+
+        """Function:  test_opt_arg_list
+
+        Description:  Test with opt_arg_list argument passed.
+
+        Arguments:
+
+        """
+
+        mock_sub.return_value = Popen()
+        mock_fetch.return_value = self.binlog_files
+
+        dataout = mysql_log_admin.fetch_binlog(
+            self.server, bin_path="./", opt_arg_list=self.opt_arg_list)
+
+        self.assertEqual([x for x in dataout], self.filehandler)
+
+    @mock.patch("mysql_log_admin.subprocess.Popen")
+    def test_binlog_files(self, mock_sub):
+
+        """Function:  test_binlog_files
+
+        Description:  Test with binlog_files argument passed.
+
+        Arguments:
+
+        """
+
+        mock_sub.return_value = Popen()
+
+        dataout = mysql_log_admin.fetch_binlog(
+            self.server, bin_path="./", binlog_files=self.binlog_files)
+
+        self.assertEqual([x for x in dataout], self.filehandler)
+
+    @mock.patch("mysql_log_admin.mysql_libs.fetch_logs")
+    @mock.patch("mysql_log_admin.subprocess.Popen")
+    def test_binpath(self, mock_sub, mock_fetch):
+
+        """Function:  test_binpath
+
+        Description:  Test with bin_path set to None.
+
+        Arguments:
+
+        """
+
+        mock_sub.return_value = Popen()
+        mock_fetch.return_value = self.binlog_files
+
+        dataout = mysql_log_admin.fetch_binlog(self.server, bin_path="./")
+
+        self.assertEqual([x for x in dataout], self.filehandler)
+
+    @unittest.skip("Known Bug:  Cannot pass None to bin_path argument")
     @mock.patch("mysql_log_admin.mysql_libs.fetch_logs")
     @mock.patch("mysql_log_admin.subprocess.Popen")
     def test_fetch_binlog(self, mock_sub, mock_fetch):
 
         """Function:  test_fetch_binlog
 
-        Description:  Test with only default argument passed.
+        Description:  Test with only default arguments passed.
 
         Arguments:
 
         """
 
-        mock_sub.return_value = "FileHandler"
+        mock_sub.return_value = Popen()
         mock_fetch.return_value = self.binlog_files
 
-        self.assertEqual(mysql_log_admin.fetch_binlog(
-            self.server, binlog_files=self.binlog_files, bin_path="./"),
-                         self.filehandler)
+        dataout = mysql_log_admin.fetch_binlog(self.server)
+
+        self.assertEqual([x for x in dataout], self.filehandler)
 
 
 if __name__ == "__main__":
