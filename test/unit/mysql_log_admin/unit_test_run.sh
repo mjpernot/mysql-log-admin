b1ccc7c324cfbe9cd773e930bc7a35e6198c3fff
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-12-18 16:45:34 +0000

    Update unit test runs

--- test/unit/mysql_log_admin/unit_test_run.sh
+++ test/unit/mysql_log_admin/unit_test_run.sh
@@ -7,6 +7,7 @@
 echo ""
 echo "Unit testing..."
 test/unit/mysql_log_admin/fetch_binlog.py
+test/unit/mysql_log_admin/fetch_log_entries.py
 test/unit/mysql_log_admin/fetch_log_pos.py
 test/unit/mysql_log_admin/find_dt_pos.py
 test/unit/mysql_log_admin/help_message.py
