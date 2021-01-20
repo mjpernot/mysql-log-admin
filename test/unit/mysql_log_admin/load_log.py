734ad8bf9946a63fa09ede25e6fe500f8ddcbbc9
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-01-20 12:48:03 +0000

    Updated test/unit/mysql_log_admin/load_log.py v2.1.1

--- test/unit/mysql_log_admin/load_log.py
+++ test/unit/mysql_log_admin/load_log.py
@@ -29,6 +29,7 @@ import mock
 # Local
 sys.path.append(os.getcwd())
 import mysql_log_admin
+import lib.gen_libs as gen_libs
 import version
 
 __version__ = version.__version__
@@ -106,6 +107,7 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_list_fail -> Test with process_logs_list function fails.
         test_no_opt_arg_lists -> Test with empty opt_arg_list list.
         test_load_log -> Test with only default arguments passed.
 
@@ -122,23 +124,43 @@ class UnitTest(unittest.TestCase):
         """
 
         self.server = Server()
+        self.proc = SubProcess()
         self.opt_arg_list = ["--force-read", "--read-from-remote-server"]
         self.args_array = {"-e": True, "-d": True, "-p": "/dir/patch",
                            "-s": True, "-t": True}
+        self.cmd_list = ["command", "options"]
+        self.status = (True, None)
+        self.status2 = (False, "Error Message")
+        self.binlog_list = ["binlog1", "binlog2"]
 
     @mock.patch("mysql_log_admin.cmds_gen.disconnect",
                 mock.Mock(return_value=True))
-    @mock.patch("mysql_log_admin.subprocess.Popen",
-                mock.Mock(return_value=SubProcess()))
-    @mock.patch("mysql_log_admin.fetch_binlog",
-                mock.Mock(return_value="Process"))
-    @mock.patch("mysql_log_admin.mysql_libs.crt_cmd",
-                mock.Mock(return_value=["command", "options"]))
-    @mock.patch("mysql_log_admin.mysql_libs.create_instance",
-                mock.Mock(return_value=Server()))
-    @mock.patch("mysql_log_admin.process_logs_list",
-                mock.Mock(return_value=["binlog1", "binlog2"]))
-    def test_no_opt_arg_lists(self):
+    @mock.patch("mysql_log_admin.process_logs_list")
+    def test_list_fail(self, mock_logs):
+
+        """Function:  test_list_fail
+
+        Description:  Test with process_logs_list function fails.
+
+        Arguments:
+
+        """
+
+        mock_logs.return_value = self.status2, self.binlog_list
+
+        with gen_libs.no_std_out():
+            self.assertFalse(mysql_log_admin.load_log(
+                self.server, self.args_array, self.opt_arg_list))
+
+    @mock.patch("mysql_log_admin.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_log_admin.subprocess.Popen")
+    @mock.patch("mysql_log_admin.fetch_binlog")
+    @mock.patch("mysql_log_admin.mysql_libs.crt_cmd")
+    @mock.patch("mysql_log_admin.mysql_libs.create_instance")
+    @mock.patch("mysql_log_admin.process_logs_list")
+    def test_no_opt_arg_lists(self, mock_logs, mock_inst, mock_cmd, mock_fetch,
+                      mock_popen):
 
         """Function:  test_no_opt_arg_lists
 
@@ -148,22 +170,24 @@ class UnitTest(unittest.TestCase):
 
         """
 
+        mock_logs.return_value = self.status, self.binlog_list
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.cmd_list
+        mock_fetch.return_value = "Process"
+        mock_popen.return_value = self.proc
+
         self.assertFalse(mysql_log_admin.load_log(
             self.server, self.args_array, []))
 
     @mock.patch("mysql_log_admin.cmds_gen.disconnect",
                 mock.Mock(return_value=True))
-    @mock.patch("mysql_log_admin.subprocess.Popen",
-                mock.Mock(return_value=SubProcess()))
-    @mock.patch("mysql_log_admin.fetch_binlog",
-                mock.Mock(return_value="Process"))
-    @mock.patch("mysql_log_admin.mysql_libs.crt_cmd",
-                mock.Mock(return_value=["command", "options"]))
-    @mock.patch("mysql_log_admin.mysql_libs.create_instance",
-                mock.Mock(return_value=Server()))
-    @mock.patch("mysql_log_admin.process_logs_list",
-                mock.Mock(return_value=["binlog1", "binlog2"]))
-    def test_load_log(self):
+    @mock.patch("mysql_log_admin.subprocess.Popen")
+    @mock.patch("mysql_log_admin.fetch_binlog")
+    @mock.patch("mysql_log_admin.mysql_libs.crt_cmd")
+    @mock.patch("mysql_log_admin.mysql_libs.create_instance")
+    @mock.patch("mysql_log_admin.process_logs_list")
+    def test_load_log(self, mock_logs, mock_inst, mock_cmd, mock_fetch,
+                      mock_popen):
 
         """Function:  test_load_log
 
@@ -172,6 +196,12 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
+
+        mock_logs.return_value = self.status, self.binlog_list
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.cmd_list
+        mock_fetch.return_value = "Process"
+        mock_popen.return_value = self.proc
 
         self.assertFalse(mysql_log_admin.load_log(
             self.server, self.args_array, self.opt_arg_list))
