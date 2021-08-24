d55035fcc8e89ea704c699e0ab4c846d004f4c45
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-08-24 17:05:14 +0100

    Update test/unit/mysql_log_admin/load_log.py v2.2.0

--- test/unit/mysql_log_admin/load_log.py
+++ test/unit/mysql_log_admin/load_log.py
@@ -42,8 +42,8 @@ class SubProcess(object):
     Description:  Class stub holder for subprocess class.
 
     Methods:
-        __init__ -> Class initialization.
-        wait -> Stub holder for subprocess subprocess.wait method.
+        __init__
+        wait
 
     """
 
@@ -79,8 +79,8 @@ class Server(object):
     Description:  Class stub holder for mysql_class.Server class.
 
     Methods:
-        __init__ -> Class initialization.
-        connect -> Connect method.
+        __init__
+        connect
 
     """
 
@@ -108,7 +108,7 @@ class Server(object):
         Description:  Connect method.
 
         Arguments:
-            (input) silent -> True|False on printing error message.
+            (input) silent
 
         """
 
@@ -127,12 +127,12 @@ class UnitTest(unittest.TestCase):
     Description:  Class which is a representation of a unit testing.
 
     Methods:
-        setUp -> Initialize testing environment.
-        test_connection_error -> Test with connection error.
-        test_connection_success -> Test with successful connection.
-        test_list_fail -> Test with process_logs_list function fails.
-        test_no_opt_arg_lists -> Test with empty opt_arg_list list.
-        test_load_log -> Test with only default arguments passed.
+        setUp
+        test_connection_error
+        test_connection_success
+        test_list_fail
+        test_no_opt_arg_lists
+        test_load_log
 
     """
 
